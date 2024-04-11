d107519e9d83ad0505e3c7fe8153bd725564b143
kavv3t <153438488+kavv3t@users.noreply.github.com>
2024-04-11 20:49:34 +0200

    Update toggleMenu.js

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -1,7 +1,9 @@
 function toggleSidebar() {
   const sidebar = document.querySelector('.sidebar');
   const header = document.querySelector('.header');
+  const rightSection = document.querySelector('.right-section');
 
   sidebar.classList.toggle('active'); 
   header.classList.toggle('active');
+  rightSection.classList.toggle('active');
 }
